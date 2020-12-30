8dbc1ec509dcd5785b573ab4116f8e0fdcc5650a
amanymous <aman.mirza9@yahoo.com>
2020-12-30 23:26:40 +0500

    completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import gql from "graphql-tag";
+import Card from "./card";
 
-const GET_BOOKMARK = gql`
+const GET_BOOKMARKS = gql`
   {
     bookmarks {
       id
@@ -12,12 +13,62 @@ const GET_BOOKMARK = gql`
   }
 `;
 
-export default function Home() {
-  const { error, loading, data } = useQuery(GET_BOOKMARK);
-  console.log(data);
+const ADD_BOOKMARK = gql`
+  mutation addBookmar($url: String!, $title: String!) {
+    addBookmark(url: $url, title: $title) {
+      id
+    }
+  }
+`;
 
-  // if (error) return <h3>{error}</h3>;
-  // if (loading) return <h3>loading</h3>;
-  // console.log(data);
-  return <h1>Hello world!</h1>;
+export default function Home() {
+  let titleField;
+  let urlField;
+
+  const { error, loading, data } = useQuery(GET_BOOKMARKS);
+  const [addBookmark] = useMutation(ADD_BOOKMARK);
+  const handleSubmit = () => {
+    console.log(titleField.value);
+    console.log(urlField.value);
+    addBookmark({
+      variables: {
+        url: urlField.value,
+        title: titleField.value,
+      },
+      refetchQueries: [{ query: GET_BOOKMARKS }],
+    });
+  };
+
+  if (error) return <h3>{error}</h3>;
+
+  if (loading) return <h3>Loading..</h3>;
+
+  return (
+    <div className="container">
+      <h2>Add New Bookmark</h2>
+      <label>
+        Enter Bookmark Tite: <br />
+        <input type="text" ref={(node) => (titleField = node)} />
+      </label>
+
+      <br />
+      <label>
+        Enter Bookmark Url: <br />
+        <input type="text" ref={(node) => (urlField = node)} />
+      </label>
+
+      <br />
+      <br />
+      <button onClick={handleSubmit}>Add Bookmark</button>
+
+      <h2>My Bookmark List</h2>
+      {/* {JSON.stringify(data.bookmarks)} */}
+
+      <div className="card-container">
+        {data.bookmarks.map((bm) => (
+          <Card url={bm.url} title={bm.title} />
+        ))}
+      </div>
+    </div>
+  );
 }
